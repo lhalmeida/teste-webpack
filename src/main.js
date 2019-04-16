97c93da7a7217bfa0fec0979c9f1d731fef0fce8
Lucas Henrique <lholiveiraa@gmail.com>
2019-04-16 19:37:14 -0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+
+import Usuario, { idade as IdadeUsuario } from './functions';
+
+Usuario.info();
+console.log(IdadeUsuario);
+
+/* //import { soma as somaFunction, sub } from './funcoes';
+//import soma from './soma';
+import * as funcoes from './funcoes';
+
+console.log(funcoes.soma (1, 2));
+console.log(funcoes.sub (3, 2));
+console.log(funcoes.mult (3, 2)); */
